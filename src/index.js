bba1a5a1325720ebed4762146b7970813cdf79dc
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-06-18 21:36:34 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import express from 'express';
-
-const PORT = 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+import setupServer from './server.js';
+setupServer();
