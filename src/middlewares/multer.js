1f8b3eece6e6e0586e21a3197f79288cf4930f96
EZH1809 <stellage.ua@gmail.com>
2024-07-21 15:20:58 +0300

    Update multer.js

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-
+import createHttpError from 'http-errors';
 import { TEMP_UPLOAD_DIR } from '../constants/index.js';
 
 const storage = multer.diskStorage({
@@ -12,4 +12,21 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+// Ограничение размера файла и фильтрация типов файлов
+const limits = {
+  fileSize: 1024 * 1024 * 5,
+};
+
+const fileFilter = (req, file, cb) => {
+  const extension = file.originalname.split('.').pop();
+  if (extension === 'exe') {
+    return cb(createHttpError(400, '.exe files are not allowed'));
+  }
+  cb(null, true);
+};
+
+export const upload = multer({
+  storage,
+  limits,
+  fileFilter,
+});
