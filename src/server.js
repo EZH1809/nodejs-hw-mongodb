a9f42a6b85ee1a57c85cf0ae5357977e48c35a56
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-07-27 07:38:37 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -25,15 +25,15 @@ export const setupServer = () => {
       },
     }),
   );
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+
   app.use(router); // Додаємо роутер до app як middleware
   //обращение к несуществующему маршруту
   app.use('*', notFoundHandler);
 
   //для обработки ошибок
   app.use(errorHandler);
-
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
